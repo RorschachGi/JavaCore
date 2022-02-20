ee583f084ca21879a5ee78802269b67c1ae4b702
RorschachGi <tumanovAccount7773@rambler.ru>
2022-02-20 16:02:57 +0400

    Lecture 1 "Variables" is completed

---
+++ src/lesson2/Variables.java
@@ -0,0 +1,23 @@
+package lesson2;
+
+public class Variables {
+    public static void main(String [] args){
+        int myInt;
+        myInt = 557;
+
+        int myIntTwo = 654;
+        short myShort = 3255;
+        long myLong = 432456877;
+
+        double myDouble = 234.21;
+        float myFloat = 2345.5f;
+
+        char myChar = 'a';
+        boolean myBoolean = true;
+
+        byte myByte = 125; //-128 to 128
+
+
+        System.out.println(myInt);
+    }
+}
