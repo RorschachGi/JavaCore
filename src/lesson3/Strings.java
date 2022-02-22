acfcb1dc15c109fa34a34125af73c8787c893356
RorschachGi <tumanovAccount7773@rambler.ru>
2022-02-22 11:16:56 +0400

    The lecture "Strings" is completed

---
+++ src/lesson3/Strings.java
@@ -0,0 +1,16 @@
+package lesson3;
+
+public class Strings {
+    public static void main (String[] args){
+
+        String str = "Hello";
+        String space = " ";
+        String name = "Ivan";
+        System.out.println(str + space + name);
+
+        int x = 10;
+
+        System.out.println(str + x);
+
+    }
+}
