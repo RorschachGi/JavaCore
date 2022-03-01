db25de947da298b2c33b29b0db66e68b4677cda4
RorschachGi <tumanovAccount7773@rambler.ru>
2022-03-01 12:19:00 +0400

    The lecture "final" is completed

---
+++ src/lesson22/finalWord.java
@@ -0,0 +1,18 @@
+package lesson22;
+
+public class finalWord {
+    public static void main(String[] args){
+        // final используется для переменных, которые не могу бить изменены ( константы )
+
+        final int X = 10;
+        System.out.println(X);
+    }
+}
+
+class Test{
+
+    // Константы внутри класса часто statiс для экономии памяти
+    public static final int CONSTANT = 0;
+
+
+}
