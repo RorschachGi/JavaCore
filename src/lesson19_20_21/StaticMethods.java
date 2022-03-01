f14b615430df8e651caffa1006eeafdc95de6b3a
RorschachGi <tumanovAccount7773@rambler.ru>
2022-03-01 12:07:14 +0400

    The lecture "static" is completed

---
+++ src/lesson19_20_21/StaticMethods.java
@@ -0,0 +1,45 @@
+package lesson19_20_21;
+
+public class StaticMethods {
+    public static void main(String[] args){
+
+        Human humanTest = new Human("Bob", 28);
+        Human humanTest2 = new Human("John", 86);
+
+        Human.printNumberOfPeople();
+
+    }
+}
+
+// Статический метод работает только со статическими переменными
+
+class Human{
+
+    private String name;
+    private int age;
+
+    private static int countPeople;
+
+    public Human(String name, int age){
+        this.name = name;
+        this.age = age;
+        countPeople++;
+    }
+
+    public void setAllFields(){
+        System.out.println(name + " " + age + " " + countPeople);
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public static void printNumberOfPeople(){
+        System.out.println(countPeople);
+    }
+
+}
