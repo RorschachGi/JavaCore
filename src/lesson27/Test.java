1208b146d17bbcf9699458506cb2fb4f38d3ef35
RorschachGi <tumanovAccount7773@rambler.ru>
2022-03-10 12:20:02 +0400

    The lecture "Packages" is completed

---
+++ src/lesson27/Test.java
@@ -0,0 +1,24 @@
+package lesson27;
+
+
+// Импортируем пакеты
+// .* - импортируем все в пакете
+import lesson27.Forest.Something.SomeClass;
+import lesson27.Forest.*;
+
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner("Aboba");
+
+        SomeClass someInstace = new SomeClass();
+
+        Tree treeOne = new Tree();
+        Squirell squirellOne = new Squirell();
+
+
+    }
+}
