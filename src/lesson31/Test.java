e96412f29ee93853d5c1563268dbdc16b1d7814d
RorschachGi <tumanovAccount7773@rambler.ru>
2022-03-18 12:06:30 +0400

    The lecture "Primitive wrapper classes" is completed

---
+++ src/lesson31/Test.java
@@ -0,0 +1,20 @@
+package lesson31;
+
+public class Test {
+    public static void main(String[] args){
+        int x = 1;
+
+        // Double, Float, Integer, Long, Short, Byte, Character, Boolean - классы обертки с методами
+
+        Integer x2 = new Integer(10); // избыточно
+        Integer.parseInt("123"); // Из строки в int
+
+        // Если необходимо обычное число, то используем примитив, если нужны методы, то класс-обертку
+
+        Integer a = 34; // Java самостоятельно упакует примитив в обертку
+
+
+
+
+    }
+}
